79e1210ff20801a5e7b853451831b33af76391f1
ashoksuryap <ashoksurya.p@gmail.com>
2020-05-11 10:35:03 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var fileContents = System.IO.File.ReadAllText(@"C:\Ashok\temp.txt");
+            var url = "https://172.22.33.55:8080/api";
+            Console.WriteLine(fileContents);
+            Console.WriteLine(url);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
